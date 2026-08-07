a112930fbb18c6bb6ea8a0136ddd8ca83167a03e
jumpsu2 <jumpsu2.whatever.dude@gmail.com>
2026-08-08 00:53:32 +0300

    dart timer consistency + cleanup

--- Content/Tiles/FatteningDartTrap.cs
+++ Content/Tiles/FatteningDartTrap.cs
@@ -7,8 +7,6 @@ using WgMod.Content.Projectiles;
 
 namespace WgMod.Content.Tiles;
 
-// This class shows off a number of less common ModTile methods. These methods help our trap tile behave like vanilla traps. 
-// In particular, hammer behavior is particularly tricky. The logic here is setup for multiple styles as well.
 public class FatteningDartTrap : ModTile
 {
     public override void SetStaticDefaults()
@@ -22,19 +20,17 @@ public class FatteningDartTrap : ModTile
         Main.tileBlockLight[Type] = true;
         Main.tileFrameImportant[Type] = true;
 
-        // These 2 AddMapEntry and GetMapOption show off multiple Map Entries per Tile. Delete GetMapOption and all but 1 of these for your own ModTile if you don't actually need it.
-        AddMapEntry(new Color(21, 179, 192), Language.GetText("MapObject.Trap"));
+        AddMapEntry(new Color(255,225,25), Language.GetText("MapObject.Trap"));
     }
 
     public override bool IsTileDangerous(int i, int j, Player player) => true;
 
     public override bool CreateDust(int i, int j, ref int type)
     {
-        type = DustID.Lihzahrd;
+        type = DustID.t_Honey;
         return true;
     }
 
-    // PlaceInWorld is needed to facilitate styles and alternates since this tile doesn't use a TileObjectData. Placing left and right based on player direction is usually done in the TileObjectData, but the specifics of that don't work for how we want this tile to work. 
     public override void PlaceInWorld(int i, int j, Item item)
     {
         Tile tile = Main.tile[i, j];
@@ -44,10 +40,7 @@ public class FatteningDartTrap : ModTile
             NetMessage.SendTileSquare(-1, Player.tileTargetX, Player.tileTargetY, 1, TileChangeType.None);
     }
 
-    // This progression matches vanilla tiles, you don't have to follow it if you don't want. Some vanilla traps don't have 6 states, only 4. This can be implemented with different logic in Slope. Making 8 directions is also easily done in a similar manner.
     static readonly int[] _frameXCycle = [2, 3, 4, 5, 1, 0];
-
-    // We can use the Slope method to override what happens when this tile is hammered.
     public override bool Slope(int i, int j)
     {
         Tile tile = Main.tile[i, j];
@@ -62,18 +55,13 @@ public class FatteningDartTrap : ModTile
     {
         Tile tile = Main.tile[i, j];
         Vector2 spawnPosition;
-        // This logic here corresponds to the orientation of the sprites in the spritesheet, change it if your tile is different in design.
         int horizontalDirection = (tile.TileFrameX == 0) ? -1 : ((tile.TileFrameX == 18) ? 1 : 0);
         int verticalDirection = (tile.TileFrameX < 36) ? 0 : ((tile.TileFrameX < 72) ? -1 : 1);
-        // Each trap style within this Tile shoots different projectiles.
-        // Wiring.CheckMech checks if the wiring cooldown has been reached. Put a longer number here for less frequent projectile spawns. 200 is the dart/flame cooldown. Spear is 90, spiky ball is 300
-        if (Wiring.CheckMech(i, j, 198))
+        if (Wiring.CheckMech(i, j, 200))
         {
-            spawnPosition = new Vector2(i * 16 + 8 + 0 * horizontalDirection, j * 16 + 9 + 0 * verticalDirection); // The extra numbers here help center the projectile spawn position if you need to.
+            spawnPosition = new Vector2(i * 16 + 8 + 0 * horizontalDirection, j * 16 + 9 + 0 * verticalDirection);
 
-            // In a real mod you should be spawning projectiles that are both hostile and friendly to do damage to both players and NPC, as Terraria traps do.
-            // Make sure to change velocity, projectile, damage, and knockback.
-            Projectile.NewProjectile(Wiring.GetProjectileSource(i, j), spawnPosition, new Vector2(horizontalDirection, verticalDirection) * 12f, ModContent.ProjectileType<FatteningDart>(), 20, 2f, Main.myPlayer);
+            Projectile.NewProjectile(Wiring.GetProjectileSource(i, j), spawnPosition, new Vector2(horizontalDirection, verticalDirection) * 12f, ModContent.ProjectileType<FatteningDart>(), 25, 2f, Main.myPlayer);
         }
     }
 }
