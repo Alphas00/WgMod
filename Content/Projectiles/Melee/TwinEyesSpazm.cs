aa73c982daf8cbd7e9b7ea2c6af3b90de47c4f25
MaimaiChubsUwU <maimaiowo12705@gmail.com>
2026-08-09 03:06:32 -0700

    Added TwinEyes

---
+++ Content/Projectiles/Melee/TwinEyesSpazm.cs
@@ -0,0 +1,5 @@
+namespace WgMod.Content.Projectiles.Melee;
+
+[Credit(ProjectRole.Programmer, Contributor.maimaichubs)]
+[Credit(ProjectRole.Artist, Contributor.PLACEHOLDER)]
+public class TwinEyesSpazm : TwinEyesRet { }
